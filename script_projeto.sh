72b0599c97039aa0667c60e76179c264dd22c070
Abhner Araújo <afga@cin.ufpe.br>
2017-07-12 22:17:07 +0000

    Docker

--- script_projeto.sh
+++ script_projeto.sh
@@ -71,4 +71,54 @@ then
 
     cd spring-petclinic;
     mvn package;
+
+fi
+
+if hash docker 2>/dev/null;
+then
+    echo "Docker OK";
+else
+    echo "Docker is not installed and is needed to proceed. Installing now..";
+    sudo apt-get update;
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D;
+    sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main';
+    sudo apt-get update;
+    apt-cache policy docker-engine;
+    sudo apt-get install -y docker-engine;
+    
+    if hash junit 2>/dev/null;
+    then
+        echo "Docker OK";
+    else
+        AMBIENT_OK=false;
+    fi
+fi
+if [ "$AMBIENT_OK" = true ];
+then
+    echo "Tudo pronto!";
+    sudo docker login;
+    echo "Criar nova imagem[I] ou usar padrão[P]?";
+
+    read imagem;
+    
+    if [ "$imagem" == "I"];
+    then
+        echo "digite o nome da imagem:";
+        read imgnome;
+        sudo docker build -t "$imgnome" .;
+        echo "usuario do docker:"
+        read user
+        sudo docker tag "$imgnome" "$user"/"$imgnome";
+        sudo docker push "$user"/"$imgnome";
+        echo "Imagem $imgnome no dockerhub";
+        
+    else
+        sudo docker build -t spring-petclininc .;
+        sudo docker tag spring-petclininc ianfireman/spring-petclininc:v2;
+        sudo docker push ianfireman/spring-petclininc:v2;
+        echo "Imagem spring-petclininc no dockerhub";
+    fi
+    
+else
+    echo "Instalação do docker foi mal sucedida"
 fi
